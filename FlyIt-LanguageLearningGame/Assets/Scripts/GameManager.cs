55f4b6af57fa775bcd6cd651a6f6db6d36877e99
Ionut <ionutputinicabz@gmail.com>
2020-09-30 15:40:37 +0200

    Implemented back-end side (GameManager + Creating Questions)

---
+++ FlyIt-LanguageLearningGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    public Question[] questions;
+    //variable that will be used to remove already used questions from the queue
+    private static List<Question> unansweredQuestions;
+
+    //the question that the user can currently answer
+
+    private Question currentQuestion;
+
+    void Start()
+    {
+        if (unansweredQuestions == null || unansweredQuestions.Count==0)
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+
+        GetRandomQuestion();
+        Debug.Log(currentQuestion.fact + " is " + currentQuestion.isTrue);
+
+    }
+
+    void GetRandomQuestion()
+    {
+        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions[randomQuestionIndex];
+
+        //removing the already answered question from the queue
+
+        unansweredQuestions.RemoveAt(randomQuestionIndex);
+    }
+
+
+}
