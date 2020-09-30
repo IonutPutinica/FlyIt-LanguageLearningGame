e39798b0ca2fa8ce2da22e488a963df24e8e15f8
Ionut <ionutputinicabz@gmail.com>
2020-09-30 16:42:12 +0200

    Started linking backend to frontend

--- FlyIt-LanguageLearningGame/Assets/Scripts/GameManager.cs
+++ FlyIt-LanguageLearningGame/Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,8 +12,21 @@ public class GameManager : MonoBehaviour
     private static List<Question> unansweredQuestions;
 
     //the question that the user can currently answer
-
     private Question currentQuestion;
+
+    [SerializeField]
+    private Text factText;
+
+    [SerializeField]
+    private Text trueAnswerText;
+
+    [SerializeField]
+    private Text falseAnswerText;
+
+
+    //storing the delay between answering a question, and displaying a new one
+    [SerializeField]
+    private float timeBetweenQuestions = 1f;
 
     void Start()
     {
@@ -20,20 +35,71 @@ public class GameManager : MonoBehaviour
             unansweredQuestions = questions.ToList<Question>();
         }
 
-        GetRandomQuestion();
-        Debug.Log(currentQuestion.fact + " is " + currentQuestion.isTrue);
+        SetCurrentQuestion();
+      
 
     }
 
-    void GetRandomQuestion()
+    void SetCurrentQuestion()
     {
         int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
         currentQuestion = unansweredQuestions[randomQuestionIndex];
 
+        factText.text=currentQuestion.fact;
+
         //removing the already answered question from the queue
 
         unansweredQuestions.RemoveAt(randomQuestionIndex);
+
+        if(currentQuestion.isTrue)
+        {
+            trueAnswerText.text = "CORRECT";
+            falseAnswerText.text = "WRONG";
+        }
+        else
+        {
+            trueAnswerText.text = "WRONG";
+            falseAnswerText.text = "CORRECT";
+        }
     }
 
+
+    IEnumerator TransitionToNextQuestion()
+    {
+        unansweredQuestions.Remove(currentQuestion);
+
+        //creating the delay
+        yield return new WaitForSeconds(timeBetweenQuestions);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void userSelectTrue ()
+    {
+        if(currentQuestion.isTrue)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("Wrong");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+    public void userSelectFalse()
+    {
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("Wrong");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
 
 }
