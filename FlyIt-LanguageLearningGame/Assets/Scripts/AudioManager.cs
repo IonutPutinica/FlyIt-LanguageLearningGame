1be7921452a335dcbce0bd87e3f18671aea9a1b4
Ionut <ionutputinicabz@gmail.com>
2020-10-03 11:44:35 +0200

    Added AudioManager script + volume slider functionality

---
+++ FlyIt-LanguageLearningGame/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource BackgroundAudio;
+
+    //default volume
+    private float musicVolume = 0.3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BackgroundAudio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BackgroundAudio.volume = musicVolume;
+    }
+
+    public void updateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
